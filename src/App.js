e1dd09bd1ea01b92b6fa611133b13ffe1dfbe59e
Keigo Kudo <keigo@Keigos-MBP.fritz.box>
2021-01-18 16:58:48 +0100

    test styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledH1 = styled.h1`
+  color: red;
+`
 
 const App = () => {
-  return <h1>test</h1>
+  return <StyledH1>test</StyledH1>
 }
 
 export default App
