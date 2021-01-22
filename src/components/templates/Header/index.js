ee16f5b8bde0e5cbc3fe55f7cd14f317ada80b97
Keigo Kudo <keigo@Keigos-MBP.fritz.box>
2021-01-22 15:54:19 +0100

    add header template

--- src/components/templates/Header/index.js
+++ src/components/templates/Header/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  height: 8rem;
+  width: 100%;
+  background: grey;
+`
 
 const Header = () => {
-  return <div>Header</div>
+  return <StyledDiv />
 }
 
 export default Header
