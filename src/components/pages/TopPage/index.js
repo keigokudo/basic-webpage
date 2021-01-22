3e9e392aa816808a40304c6aaec1ee34d671c64a
Keigo Kudo <keigo@Keigos-MBP.fritz.box>
2021-01-22 15:55:02 +0100

    test Link component

--- src/components/pages/TopPage/index.js
+++ src/components/pages/TopPage/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
+import Header from '../../templates/Header'
 import InvertedTriangle from '../../templates/InvertedTriangle'
 
+import Link from '../../atoms/Link'
+
 const TopPage = () => {
-  return <InvertedTriangle />
+  return (
+    <Fragment>
+      <Header />
+      <Link url="https://www.w3schools.com/tags/tag_a.asp" name="Link" />
+      <InvertedTriangle />
+    </Fragment>
+  )
 }
 
 export default TopPage
