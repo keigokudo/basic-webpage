163b5bac471db6f54bc4da8e743a7ee3e9608d94
Keigo Kudo <keigo@Keigos-MBP.fritz.box>
2021-01-23 18:36:49 +0100

    add logo section

--- src/components/templates/Header/index.js
+++ src/components/templates/Header/index.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const StyledDiv = styled.div`
   height: 8rem;
   width: 100%;
-  background: grey;
 `
 
-const Header = () => {
-  return <StyledDiv />
+const Container = styled.div`
+  display: flex;
+`
+
+const LogoSection = styled.section`
+  height: 6rem;
+  width: 6rem;
+  margin: 1rem 5rem 1rem 1rem;
+`
+
+const NavLinkSection = styled.section``
+
+const Header = (props) => {
+  const { logo, navLinks } = props
+
+  return (
+    <StyledDiv>
+      <Container>
+        <LogoSection>{logo}</LogoSection>
+        <NavLinkSection>{navLinks}</NavLinkSection>
+      </Container>
+    </StyledDiv>
+  )
+}
+
+Header.propTypes = {
+  logo: PropTypes.element,
+  navLinks: PropTypes.arrayOf(PropTypes.element),
 }
 
 export default Header
