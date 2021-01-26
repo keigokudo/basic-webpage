f0b16d363e40c0863ce92947c25ba60650f7d220
keigokudo <keigokudo55@googlemail.com>
2021-01-26 13:59:53 +0100

    set pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import TopPage from './components/pages/TopPage'
+import About from './components/pages/About'
 
 const App = () => {
-  return <TopPage />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={TopPage} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
