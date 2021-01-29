d98e57d242e3404ea14d27c7eec12b58731375da
keigokudo <keigokudo55@googlemail.com>
2021-01-29 10:49:07 +0100

    add color scheme

---
+++ src/color.js
@@ -0,0 +1,7 @@
+export const Colors = {
+  FloralWhite: '#FFFCF2',
+  PaleSilver: '#CCC5B9',
+  BlackOlive: '#403D39',
+  EerieBlack: '#252422',
+  FlameOrange: '#EB5E28',
+}
--- src/components/atoms/Link/index.js
+++ src/components/atoms/Link/index.js
@@ -1,26 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { Link as RrdLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
+
+import { Colors } from 'color.js'
 
 const StyledRrdLink = styled(RrdLink)`
   text-decoration: none;
   font-size: 2rem;
-  padding: 1rem;
+  margin: 0 1rem;
+  color: ${Colors.BlackOlive};
 
   &:hover {
-    opacity: 0.5;
+    opacity: 0.7;
   }
 `
 
 const Link = (props) => {
-  const { name, url } = props
-  return <StyledRrdLink to={url}>{name}</StyledRrdLink>
+  const { name, to } = props
+  return <StyledRrdLink to={to}>{name}</StyledRrdLink>
 }
 
 Link.propTypes = {
   name: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 }
 
 export default Link
