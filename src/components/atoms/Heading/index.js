ad1d9c09a16b372fe9cd3f7350e57ca4fbe68a68
keigokudo <keigokudo55@googlemail.com>
2021-02-01 13:56:02 +0100

    make heading tag flexible

--- src/components/atoms/Heading/index.js
+++ src/components/atoms/Heading/index.js
@@ -2,19 +2,31 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
+import { Colors } from 'color.js'
+
 const StyledHeading = styled.h1`
   font-size: ${(props) => (props.size ? props.size : '5rem')};
   white-space: pre-wrap;
+  color: ${Colors.BlackOlive};
 `
 
 const Heading = (props) => {
-  const { title, size } = props
-  return <StyledHeading size={size}>{title}</StyledHeading>
+  const { title, size, level } = props
+  return (
+    <StyledHeading size={size} as={`h${level}`}>
+      {title}
+    </StyledHeading>
+  )
 }
 
 Heading.propTypes = {
   title: PropTypes.string.isRequired,
   size: PropTypes.string,
+  level: PropTypes.string,
+}
+
+Heading.defaultProps = {
+  level: 1,
 }
 
 export default Heading
