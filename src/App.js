e0a8942131a5b4922c9c578b5ed0916aa0a4e2a0
keigokudo <keigokudo55@googlemail.com>
2021-02-03 13:43:18 +0100

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import Home from 'components/pages/Home'
 import About from 'components/pages/About'
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+    </Switch>
   )
 }
 
