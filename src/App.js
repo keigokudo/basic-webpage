c8205f2de9f24d30126d42e252626d49931f08ff
keigokudo <keigokudo55@googlemail.com>
2021-02-06 10:07:37 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from 'components/pages/Home'
 import About from 'components/pages/About'
